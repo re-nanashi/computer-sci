77a9ec8e3aca12889e0a7a77fc8cae411de127c9
re-nanashi <jfabro3124@gmail.com>
2021-10-17 14:35:36 +0800

    sort list

--- how-to-code:simple-data/5b/arrange_images.rkt
+++ how-to-code:simple-data/5b/arrange_images.rkt
@@ -1,18 +1,25 @@
-(require 2htdp/image)
+;; ====================
+;; Constants:
 
-(define IMAGE0 (square 1 "solid" "white"))
-(define IMAGE1 (square 2 "solid" "white"))
-(define IMAGE2 (square 3 "solid" "white"))
-(define IMAGE3 (square 4 "solid" "white"))
+;; Image
+;; interp. an image
+(define IMAGE1 (rectangle 3 4 "solid" "black"))
+(define IMAGE2 (rectangle 6 2 "solid" "pink"))
+(define IMAGE3 (rectangle 5 4 "solid" "red"))
+(define IMAGE4 (rectangle 5 5 "solid" "blue"))
 
-;; ListOfImages is one of:
+#; 
+(define (fn-for-image img)
+  (... img))
+
+;; ListOfImage is one of:
 ;; - empty
-;; - (cons Image ListOfImages)
+;; - (cons Image ListOfImage)
 ;; interp. list of images
 (define LOI0 empty)
 (define LOI1 (cons IMAGE1 empty))
-(define LOI2 (cons IMAGE3 (cons IMAGE1 empty))) 
-(define LOI3 (cons IMAGE3 (cons IMAGE1 (cons IMAGE2 empty)))) 
+(define LOI2 (cons IMAGE2 (cons IMAGE1 empty)))
+(define LOI3 (cons IMAGE4 (cons IMAGE3 (cons IMAGE1 empty))))
 
 #;
 (define (fn-for-loi loi)
@@ -24,36 +31,82 @@
 ;; Template rules used:
 ;; - one of: 2 cases
 ;; - atomic distinct: empty
-;; - compound: (cons Image ListOfImages)
-;; - self-reference: (rest loi) is ListOfImages
+;; - compound: (cons Image ListOfImage)
+;; - reference: (first loi) is Image
+;; - self-reference (rest loi) is ListOfImage
 
+;; ====================
+;; Functions:
 
-;; ListOfImages -> Image
-;; produces a list that has been arranged in increasing order given an unordered list
-(check-expect (arrangeImages LOI2) (beside IMAGE1 IMAGE3 empty-image))
-(check-expect (arrangeImages LOI3) (beside IMAGE1 IMAGE2 IMAGE3 empty-image))
+;; arrangeImages( ListOfImage ) -> Image
+;; produce an image that lays out a list in sorted order (increasing)
+(check-expect (arrangeImages LOI0) empty-image)
+(check-expect (arrangeImages LOI1) (beside IMAGE1 empty-image))
+(check-expect (arrangeImages LOI2) (beside IMAGE2 IMAGE1 empty-image))
+(check-expect (arrangeImages LOI3) (beside IMAGE1 IMAGE3 IMAGE4 empty-image))
 
-;(define (arrangeImages loi) loi) ;stub
+;(define (arrangeImages loi) empty-image) ;stub
 
 (define (arrangeImages loi)
-  (renderImage (sortList loi)))
+  (renderImages (sortList loi)))
 
+;; renderImages( ListOfImage ) -> Image
+;; render the list to images given a sorted list
+(check-expect (renderImages LOI0) empty-image) 
+(check-expect (renderImages LOI1) (beside IMAGE1 empty-image)) 
+(check-expect (renderImages LOI2) (beside IMAGE2 IMAGE1 empty-image)) 
 
-;; ListOfImages -> Image
-;; render images beside each other
-(check-expect (renderImage LOI0) empty-image)
-(check-expect (renderImage LOI2) (beside IMAGE3 IMAGE1 empty-image))
+;(define (renderImages loi) empty-image) ;stub
+; <template from ListOfImage>
 
-;(define (renderImage loi) empty-image) ; stub
-
-(define (renderImage loi)
+(define (renderImages loi)
   (cond [(empty? loi) empty-image]
         [else
           (beside (first loi)
-                  (renderImage (rest loi)))]))
+                  (renderImages (rest loi)))]))
 
-(define IMAGE1 (square 2 "solid" "white"))
-(define IMAGE2 (square 3 "solid" "white"))
-(define IMAGE3 (square 4 "solid" "white"))
+;; sortList( ListOfImage ) -> ListOfImage
+;; sort the given list into increasing order
+(check-expect (sortList empty) empty)
+(check-expect (sortList LOI1) (cons IMAGE1 empty))
+(check-expect (sortList LOI2) (cons IMAGE2 (cons IMAGE1 empty)))
+(check-expect (sortList LOI3) (cons IMAGE1 (cons IMAGE3 (cons IMAGE4 empty))))
 
-;(check-expect (arrangeImages LOI2) (cons IMAGE1 (cons IMAGE2 (cons IMAGE3 empty)))) ; sort check-expect
+;(define (sortList loi) loi) ; stub
+; <template from ListOfImage>
+
+(define (sortList loi)
+  (cond [(empty? loi) empty]
+        [else
+          (insert (first loi)
+                  (sortList (rest loi)))]))  ; list will be sorted
+
+;; insert( IMAGE ListOfImage ) -> ListOfImage
+;; insert the an image into a list by increasing order
+;; !!! ASSUME: that ListOfImage is already sorted
+(check-expect (insert IMAGE1 LOI0) LOI1)
+(check-expect (insert IMAGE2 LOI1) (cons IMAGE2 (cons IMAGE1 empty)))
+(check-expect (insert IMAGE3 (cons IMAGE2 (cons IMAGE1 empty))) (cons IMAGE2 (cons IMAGE1 (cons IMAGE3 empty))))
+
+;(define (insert img loi) loi) ;stub
+
+(define (insert img loi)
+  (cond [(empty? loi) (cons img empty)]
+        [else
+          (if (larger? img (first loi))
+            ;; if img is larger put first in front of the list and recurse until sorted
+            (cons (first loi) (insert img (rest loi)))
+            ;; if (first loi) is larger, put img in front of the list
+            (cons img loi))]))
+
+;; larger? (Image Image) -> Boolean
+;; produce true if Image1 is larger than Image2
+(check-expect (larger? IMAGE2 IMAGE1) false)
+(check-expect (larger? IMAGE1 IMAGE3) false)
+(check-expect (larger? IMAGE4 IMAGE3) true)
+
+
+(define (larger? img1 img2)
+  (> (* (image-width img1) (image-height img1)) 
+     (* (image-width img2) (image-height img2))))
+
