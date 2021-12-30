a1ed81379f5e6d3da567463ff210aa14f2340b46
re-nanashi <jfabro3124@gmail.com>
2021-12-30 22:19:31 +0800

    Homework 4 with challenge problem

--- programming-languages-part-b/week_1/homework/hw4.rkt
+++ programming-languages-part-b/week_1/homework/hw4.rkt
@@ -2,21 +2,14 @@
 
 (provide (all-defined-out)) 
 
-; Integer Integer Integer -> (listof Integer)
-; produce a list of numbers from lo to hi with interval st
 (define (sequence lo hi st)
   (cond [(> lo hi) empty]
         [else 
-          (cons lo 
-                (sequence (+ lo st) hi st))]))
+          (cons lo (sequence (+ lo st) hi st))]))
 
-; (listof String) String -> (listof String)
-; returns a list of string with suffix attached to all elements of the list
 (define (string-append-map los s)
   (map (lambda (str) (string-append str s)) los))
 
-; (listof x) Integer -> x
-; produce the ith element of the list by dividing n to length of list
 (define (list-nth-mod xs n)
   (cond [(negative? n) (error "list-nth-mod: negative number")]
         [(empty? xs) (error "list-nth-mod: empty list")]
@@ -45,6 +38,45 @@
                       (lambda () (f (cdr (x))))))])
     (lambda () (f s))))
 
-; !!!
 (define (cycle-lists xs ys)
-  (cond []))
+  (letrec ([lx (length xs)]
+           [ly (length ys)]
+           [f (lambda (x y) (cons (cons (list-nth-mod xs (remainder x lx)) 
+                                        (list-nth-mod ys (remainder y ly))) 
+                                  (lambda () (f (+ x 1) (+ y 1)))))])
+    (lambda () (f 0 0))))
+
+(define (vector-assoc v vec) 
+  (letrec ([vl (vector-length vec)]
+           [f (lambda (x)
+                (cond [(equal? vl x) false]
+                      [(let ([e (vector-ref vec x)]) 
+                         (and (pair? e) 
+                              (equal? v (car e))))
+                       (vector-ref vec x)]
+                      [else (f (add1 x))]))]) 
+    (f 0)))
+
+(define (cached-assoc xs n)
+  (letrec ([cache (make-vector n #f)]
+           [itr 0]
+           [f (lambda (x) 
+                (let ([ans (vector-assoc x cache)])
+                  (if ans 
+                    ans
+                    (let ([new-ans (assoc x xs)])
+                     (begin 
+                      (set! itr (if (equal? itr n) 0 (add1 itr)))
+                      (vector-set! cache itr new-ans)
+                      new-ans)))))])
+    f))
+
+(define-syntax while-less
+  (syntax-rules (do)
+    [(while-less e1 do e2)
+     (let ([x e1])
+       (letrec ([f (lambda () 
+                     (if (equal? x e2) 
+                       #t 
+                       (begin e2 (f))))])
+         (f)))]))
