77d982108af6b84efc7dcf90960d9634556f65e2
re-nanashi <jfabro3124@gmail.com>
2022-01-08 09:48:44 +0800

    Problem 3

--- programming-languages-part-b/week_2/homework/hw5.rkt
+++ programming-languages-part-b/week_2/homework/hw5.rkt
@@ -124,7 +124,10 @@
       e2
       (mlet (caar lstlst) (cdar lstlst) (mlet* (cdr lstlst) e2))))
 
-(define (ifeq e1 e2 e3 e4) "CHANGE")
+(define (ifeq e1 e2 e3 e4) 
+  (mlet* (list (cons "_x" e1) (cons "_y" e2))
+         (ifgreater (var "_x") (var "_y") e4 
+                    (ifgreater (var "_y") (var "_x") e4 e3))))
 
 ;; Problem 4
 
